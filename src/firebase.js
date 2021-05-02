c044777dbe5acafd99639614a2510b8b0bcbd8a1
Catherine Dawe <dawe.cm@gmail.com>
2021-05-02 13:26:54 -0400

    deleted files

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA4vgTa3UYS-xrGG4FB2lEZSnJNYiRr-x0",
+  authDomain: "netflix-clone-9223e.firebaseapp.com",
+  projectId: "netflix-clone-9223e",
+  storageBucket: "netflix-clone-9223e.appspot.com",
+  messagingSenderId: "90714820264",
+  appId: "1:90714820264:web:538d5e47ca60b0c5fd3c9d",
+  measurementId: "G-32G7RRS548",
+};
