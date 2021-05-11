0e34da5b1790673990225081efcd6da623cf58eb
Catherine Dawe <dawe.cm@gmail.com>
2021-05-11 11:03:15 -0400

    add router to app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import HomeScreen from "./components/HomeScreen";
+import LoginScreen from "./components/LoginScreen";
 import "./stylesheets/App.scss";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
+  const user = null;
   return (
     <div className="app">
-      <HomeScreen />
+      <Router>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Switch>
+            <Route exact path="/">
+              <HomeScreen />
+            </Route>
+          </Switch>
+        )}
+      </Router>
     </div>
   );
 }
