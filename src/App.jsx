c132971f4ff6c1ea8470d61575e8df142ec70e98
Catherine Dawe <dawe.cm@gmail.com>
2021-05-13 11:37:35 -0400

    set up redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import HomeScreen from "./components/HomeScreen";
 import LoginScreen from "./components/LoginScreen";
+import ProfileScreen from "./components/ProfileScreen";
 import "./stylesheets/App.scss";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import { auth } from "./firebase/config";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout } from "./features/userSlice";
+import { selectUser } from "./features/userSlice";
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+      } else {
+        dispatch(logout);
+      }
+    });
+    return unsubscribe;
+  }, [dispatch]);
+
   return (
     <div className="app">
       <Router>
@@ -13,6 +35,9 @@ function App() {
           <LoginScreen />
         ) : (
           <Switch>
+            <Route path="/profile">
+              <ProfileScreen />
+            </Route>
             <Route exact path="/">
               <HomeScreen />
             </Route>
