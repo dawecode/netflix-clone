1325346edaf94eec0ad9e2a55a01141148718907
Catherine Dawe <dawe.cm@gmail.com>
2021-05-13 11:44:20 -0400

    add signUp logic

--- src/components/SignupScreen.jsx
+++ src/components/SignupScreen.jsx
@@ -4,13 +4,36 @@ import { auth } from "../firebase/config";
 function SignupScreen() {
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
+
   const register = (e) => {
     e.preventDefault();
-    auth.createUserWithEmailAndPassword();
+
+    auth
+      .createUserWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
   };
 
   const signIn = (e) => {
     e.preventDefault();
+    auth
+      .signInWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
   };
 
   return (
